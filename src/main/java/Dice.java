a378451a05017c660d648d4630eee939a1d71d4d
sputnika <sputnika91@gmail.com>
2019-06-08 09:49:46 -0400

    update

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,12 @@
 public class Dice {
+private int numberOfDice ;
 
+    public static void main (String [] args){
+    }
+
+    public Dice (int numberOfDice){
+        this.numberOfDice = numberOfDice;
+        int roll = (int) (Math.random() * 6 +1);
+    }
 
 }
